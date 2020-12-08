916ec4d9726c1ef6f063f5b303d66e564b6e031c
João Paludo <jas.paludo@gmail.com>
2020-12-08 15:58:35 -0300

    Style the landing page for unauthorized requests

--- app/core/views.py
+++ app/core/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.views import LoginView
 
 class IndexView(LoginView):
     """Home page view of the application"""
-    template_name = 'core/index_unauthenticated.html'
+    template_name = 'core/index.html'
     redirect_authenticated_user = True
