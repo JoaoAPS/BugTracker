600cc976fff143acfd7d629461227ddef6d3ca8b
João Paludo <jas.paludo@gmail.com>
2020-12-10 16:12:01 -0300

    Style member register page

--- app/members/forms.py
+++ app/members/forms.py
@@ -4,9 +4,26 @@ from .models import Member
 
 
 class MemberCreateForm(forms.ModelForm):
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput
+    )
+
     class Meta:
         model = Member
-        fields = ['name', 'email', 'password', 'is_superuser']
+        fields = ['name', 'email', 'password']
+
         widgets = {
-            'password': forms.PasswordInput
+            'password': forms.PasswordInput,
         }
+
+    def clean(self):
+        """Check the two password inputs are equal"""
+        cleaned_data = super().clean()
+
+        if cleaned_data.get("password") != cleaned_data.get("password2"):
+            error = forms.ValidationError("The two passwords must match")
+            self.add_error('password', error)
+            self.add_error('password2', error)
+
+        return cleaned_data
