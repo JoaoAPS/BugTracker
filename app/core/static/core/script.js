dfc5263f2922e818a8558553b83de00510d63c32
João Paludo <jas.paludo@gmail.com>
2020-12-12 10:00:07 -0300

    Multiple select can be selected with single click

---
+++ app/core/static/core/script.js
@@ -0,0 +1,14 @@
+// Toggle "select multiple" selection by simply clicking
+window.onmousedown = function (e) {
+    var el = e.target;
+    if (el.tagName.toLowerCase() == 'option' && el.parentNode.hasAttribute('multiple')) {
+        e.preventDefault();
+
+        if (el.hasAttribute('selected')) el.removeAttribute('selected');
+        else el.setAttribute('selected', '');
+
+        // hack to correct buggy behavior
+        var select = el.parentNode.cloneNode(true);
+        el.parentNode.parentNode.replaceChild(select, el.parentNode);
+    }
+}
