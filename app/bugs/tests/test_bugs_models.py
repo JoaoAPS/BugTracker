29d881b1132fbce083c64d420cfd035ad3db52b4
João Paludo <jas.paludo@gmail.com>
2020-12-14 13:31:59 -0300

    Update project and bug tests

--- app/bugs/tests/test_bugs_models.py
+++ app/bugs/tests/test_bugs_models.py
@@ -8,19 +8,62 @@ from projects.models import Project
 class BugModelTests(TestCase):
     """Test the bug model"""
 
-    def test_bug_set_status(self):
-        """Test the set_status project method"""
-        member = get_user_model().objects.create_user(
+    def setUp(self):
+        self.member = get_user_model().objects.create_user(
             name='Test Member',
             email='test@gotmail.com',
             password='testpass'
         )
-        proj = Project.objects.create(title='Test')
+        self.proj = Project.objects.create(title='Test')
+        self.bug = Bug.objects.create(
+            title='Test', project=self.proj, creator=self.member
+        )
 
-        bug = Bug.objects.create(title='Test', project=proj, creator=member)
+    def test_bug_status_enums(self):
+        """Test the status enums are set up correctly"""
+        for active_status in Bug.ACTIVE_STATUS:
+            self.assertIn(active_status, Bug.POSSIBLE_STATUS)
 
-        bug.set_status('FIXED')
-        self.assertEqual(bug.status, 'FIXED')
+        self.assertIn(Bug.WAITING_STATUS, Bug.POSSIBLE_STATUS)
+        self.assertIn(Bug.WORKING_STATUS, Bug.POSSIBLE_STATUS)
+
+        for status in Bug.POSSIBLE_STATUS:
+            self.assertIn(status, Bug.STATUS_CLASSES.keys())
+
+    def test_bug_set_status(self):
+        """Test the set_status method"""
+        self.bug.set_status('FIXED')
+        self.assertEqual(self.bug.status, 'FIXED')
 
         with self.assertRaises(ValueError):
-            proj.set_status('NON_EXISTING_ENUM')
+            self.bug.set_status('NON_EXISTING_ENUM')
+
+    def test_bug_status_tuples(self):
+        """Test the status_tuples property"""
+        for st in self.bug.status_tuples:
+            self.assertEqual(Bug.STATUS_CLASSES[st[0]], st[1])
+
+    def test_get_active(self):
+        """Test the get_active class method"""
+        b1 = self.bug
+        b2 = Bug.objects.create(
+            title='Test2',
+            project=self.proj,
+            creator=self.member,
+            _status='BEING WORKED'
+        )
+        Bug.objects.create(
+            title='Test3',
+            project=self.proj,
+            creator=self.member,
+            _status='CLOSED'
+        )
+        Bug.objects.create(
+            title='Test4',
+            project=self.proj,
+            creator=self.member,
+            _status='FIXED'
+        )
+        actives = Bug.get_active().order_by('title')
+
+        self.assertQuerysetEqual(set(actives), {str(b1), str(b2)})
--- app/projects/tests/test_projects_models.py
+++ app/projects/tests/test_projects_models.py
@@ -6,12 +6,36 @@ from projects.models import Project
 class ProjectModelTests(TestCase):
     """Test the project model"""
 
+    def setUp(self):
+        self.project = Project.objects.create(title='Test')
+
+    def test_project_status_enums(self):
+        """Test the status enums are set up correctly"""
+        for active_status in Project.ACTIVE_STATUS:
+            self.assertIn(active_status, Project.POSSIBLE_STATUS)
+
+        for status in Project.POSSIBLE_STATUS:
+            self.assertIn(status, Project.STATUS_CLASSES.keys())
+
     def test_project_set_status(self):
         """Test the set_status project method"""
-        proj = Project.objects.create(title='Test')
-
-        proj.set_status('ON-GOING')
-        self.assertEqual(proj.status, 'ON-GOING')
+        self.project.set_status('ON-GOING')
+        self.assertEqual(self.project.status, 'ON-GOING')
 
         with self.assertRaises(ValueError):
-            proj.set_status('NON_EXISTING_ENUM')
+            self.project.set_status('NON_EXISTING_ENUM')
+
+    def test_project_status_tuples(self):
+        """Test the status_tuples property"""
+        for st in self.project.status_tuples:
+            self.assertEqual(Project.STATUS_CLASSES[st[0]], st[1])
+
+    def test_get_active(self):
+        """Test the get_active class method"""
+        Project.objects.create(title='Test2', _status='PAUSED')
+        Project.objects.create(title='Test3', _status='CLOSED')
+        Project.objects.create(title='Test4', _status='FINISHED')
+
+        actives = Project.get_active()
+
+        self.assertQuerysetEqual(set(actives), {str(self.project)})
